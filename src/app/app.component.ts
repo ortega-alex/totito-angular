d4e71cee71c832a3a40064ab094fb26fa78aabeb
Alex ortega <mortegalex27@ocacall.com>
2020-02-24 17:21:37 -0600

    init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { trigger, style, state, animate, transition } from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    animations: [
+        trigger('jugadorBoton', [
+            state('inactive', style({
+                'border-bottom': 'solid 0px white'
+            })),
+            state('active', style({
+                'border-bottom': 'solid 2px #26a69a'
+            })),
+            transition('inactive => active', animate('100ms ease-in')),
+            transition('active => inactive', animate('100ms ease-in'))
+        ]),
+        trigger('maquinaBoton', [
+            state('inactive', style({
+                'border-bottom': 'solid 0px white'
+            })),
+            state('active', style({
+                'border-bottom': 'solid 2px #26a69a'
+            })),
+            transition('inactive => active', animate('100ms ease-in')),
+            transition('active => inactive', animate('100ms ease-in'))
+        ])
+    ]
 })
+
 export class AppComponent {
-  title = 'totito';
+    public stateJugador: string = 'active';
+    public stateMaquina: string = 'inactive';
+    public jugadas = [
+        {value : "X", usuario: ""},
+        {value : "O", usuario: ""},
+        {value : "X", usuario: ""},
+        {value : "O", usuario: ""},
+        {value : "", usuario: ""},
+        {value : "", usuario: ""},
+        {value : "", usuario: ""},
+        {value : "", usuario: ""},
+        {value : "", usuario: ""}
+    ]    
+
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+
+    togleBotton() {
+        this.stateJugador = this.stateJugador === 'active' ? 'inactive' : 'active';
+        this.stateMaquina = this.stateMaquina === 'active' ? 'inactive' : 'active';
+    }
 }
